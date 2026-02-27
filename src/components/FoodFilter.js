9923107d91ee1a5ad9b9e19a7f08373c456e79f3
Prabha8859 <prabha.singh@gmail.com>
2026-02-27 13:16:53 +0530

    solve conflik

--- src/components/FoodFilter.js
+++ src/components/FoodFilter.js
@@ -9,7 +9,7 @@ import {
   Clock,
   Smile,
   Timer,
-  UtensilsCross,
+  UtensilsCrossed,
 } from "lucide-react";
 
 // A reusable dropdown component for filters
@@ -129,7 +129,7 @@ export default function FoodFilter({ filters, setFilters }) {
 
         {/* Cuisine Search */}
         <div className="relative flex-grow min-w-[150px]">
-          <UtensilsCross className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400" />
+          <UtensilsCrossed className="absolute left-3 top-1/2 -translate-y-1/2 h-5 w-5 text-gray-400" />
           <input
             type="text"
             name="cuisine"
